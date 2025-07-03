8665ff235f456329f9087172a6e73f3e2f66ec56
STEyPan <s.t.e.y.p.a.n@mail.ru>
2025-07-03 12:16:35 +0300

    Домашнее задание №35 - 10% (Create TABLES - 75%)

--- src/homework/DZ35/create_BD_TB.sql
+++ src/homework/DZ35/create_BD_TB.sql
@@ -1,15 +1,24 @@
--- DROP TABLE "Faculties";
+-- DROP TABLE "Faculties", "Departaments";
 
 -- CREATE TABLE "Departaments" (
 -- 	id uuid NOT NULL DEFAULT gen_random_uuid(),
 -- 	financing numeric NOT NULL DEFAULT(0) CHECK (Financing >= 0),
--- 	name varchar(100) NOT NULL UNIQUE CHECK (name <> ''),
+-- 	name text NOT NULL UNIQUE CHECK (name <> ''),
 -- 	PRIMARY KEY (id)
 -- )
 
 -- CREATE TABLE "Faculties" (
 -- 	id uuid NOT NULL DEFAULT gen_random_uuid(),
--- 	dean varchar(255) NOT NULL CHECK (dean <> ''),
--- 	name varchar(100) UNIQUE NOT NULL CHECK (name <> ''),
+-- 	dean text NOT NULL CHECK (dean <> ''),
+-- 	name text UNIQUE NOT NULL CHECK (name <> ''),
 -- 	PRIMARY KEY (id)
 -- )
+
+-- CREATE TABLE "Groups" (
+-- 	id uuid NOT NULL DEFAULT gen_random_uuid(),
+-- 	name text UNIQUE NOT NULL CHECK (name <> ''),
+-- 	rating int NOT NULL CHECK (rating >= 0 AND rating <=5),
+-- 	year int NOT NULL CHECK (year > 0 AND year <= 5),
+-- 	PRIMARY KEY (id)
+-- )
+
