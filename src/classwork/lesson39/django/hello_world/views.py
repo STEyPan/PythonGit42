cea7dc5bd67742f6192530cd665dc0997f4bbad2
Кротков Степан <s.t.e.y.p.a.n@mail.ru>
2025-09-02 21:33:38 +0300

    Django Перезагрузка

--- src/classwork/lesson39/django/hello_world/views.py
+++ src/classwork/lesson39/django/hello_world/views.py
@@ -1,23 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest, JsonResponse
 
 # Create your views here.
 
-def index(request):
+def index(request: HttpRequest):
+    user_agent = request.META["HTTP_USER_AGENT"]
+    user_addr = request.META["REMOTE_ADDR"]
+    request_method = request.META["REQUEST_METHOD"]
     response = HttpResponse()
+
     response.write(f"<h1>Это страница исходная</h1>"
                         f"<a href={"hello"}> Перейти на страницу Hello</a>"
                         f"</br>"
-                        f"<a href={"about"}> Перейти на страницу About</a>")
+                        f"<a href={"about"}> Перейти на страницу About</a>"
+                        f"</br>"
+                        f"User agent: {user_agent} </br>"
+                        f"User host: {user_addr} </br>"
+                        f"Request method: {request_method} </br>"
+                        f"<a href={"user"}> Перейти на страницу User</a>")
 
     # response.status_code = 200
-    # response.headers["content-type"] = "text/html; charset=utf-8"
+    response.headers["content-type"] = "text/html; charset=utf-8"
 
     return response
 
 def hello_world(request):
     return HttpResponse(f"<h1>Hello World</h1>"
                         f"<a href={"../"}>Назад</a>")
+
+def user(request: HttpRequest, name: str = "UNKNOWN"):
+    return HttpResponse(f"<h1>Hello {name}</h1>"
+                        f"<a href={"../"}>Назад</a>")
+
+def agent(request: HttpRequest):
+    name = request.GET.get("name")
+    return JsonResponse({"name" : "name", "data" : "json"})
+    # return HttpResponse(f"<h1>Hello {name}</h1>"
+    #                     f"<a href={"../"}>Назад</a>")
 
 def about_us(request):
     style = 'color:red;'
